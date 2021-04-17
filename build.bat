d19c3067189f62bad58baa5aa6fbbeb3692d1b09
Krzem <krzem5.dev@gmail.com>
2021-04-17 13:57:19 +0200

    Push Update 04/17/2021, 13:57:12

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
-echo off
-echo NUL>_.class&&del /s /f /q *.class
+@echo off
 cls
-javac -cp com/krzem/ds4_key_mapper/modules/jna-4.0.0.jar;com/krzem/ds4_key_mapper/modules/purejavahidapi.jar; com/krzem/ds4_key_mapper/Main.java&&java -cp com/krzem/ds4_key_mapper/modules/jna-4.0.0.jar;com/krzem/ds4_key_mapper/modules/purejavahidapi.jar; com/krzem/ds4_key_mapper/Main key.xml
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+python build.py %*
